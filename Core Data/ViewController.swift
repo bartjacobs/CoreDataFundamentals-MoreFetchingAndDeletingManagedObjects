ebefe181bfa7b84c13b9014fe13b4560517f2c8c
Bart Jacobs <developer@bartjacobs.com>
2016-01-03 10:53:49 +0100

    Project setup

---
+++ Core Data/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Core Data
+//
+//  Created by Bart Jacobs on 03/01/16.
+//  Copyright © 2016 Bart Jacobs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,7 @@
+### Core Data Fundamentals: Setting Up the Core Data Stack
+
+#### Author: Bart Jacobs
+
+In the [previous article](http://bartjacobs.com/core-data-fundamentals-core-data-stack/), you learned about the Core Data stack and the classes involved. In this article, we get our hands dirty by setting up the Core Data stack in an iOS application. We also take a look at the application template Xcode provides us with.
+
+**Read this article on the [blog](http://bartjacobs.com/core-data-fundamentals-setting-up-the-core-data-stack)**.
